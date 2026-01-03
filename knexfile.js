3e5ebc39fcfac6dadff243e7b6f146b346884161
Mohammed SLIMANI <mslimani62@yahoo.fr>
2026-01-03 20:16:57 +0100

    test6 chatgpt

--- knexfile.js
+++ knexfile.js
@@ -5,12 +5,22 @@ module.exports = {
   // pour vercel connexion:process.env.DB_URL
   // Pour le local il faut mettre tous
 
-  connection: process.env.DB_URL,
+  // connection: process.env.DB_URL,
   // connection: {
   //   user: process.env.DB_USER,
   //   database: process.env.DB_DATABASE,
   //   password: process.env.DB_PASSWORD,
   // },
+
+  connection: {
+    connectionString:
+      process.env.POSTGRES_URL_NON_POOLING || process.env.POSTGRES_URL,
+    ssl: { rejectUnauthorized: false },
+  },
+  pool: {
+    min: 0,
+    max: 1,
+  },
 
   migrations: {
     tableName: "knex_migrations",
